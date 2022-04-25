22c62240c3e8b016d05cd326ceb862ee03778a5e
prachiwabale03 <104131488+prachiwabale03@users.noreply.github.com>
2022-04-25 14:24:34 +0530

    Create Task2_EmpDetails.ts

---
+++ TypeScript/Day1/Task2_EmpDetails.ts
@@ -0,0 +1,8 @@
+let EmpId:number=54678;
+let EmpName:string="Scott";
+let EmployeeJob:string="Manager";
+let IsPermanentEmployee:Boolean=true;
+let EmpSalary:string="INR.45000";
+let EmpDeptNo:number=10;
+
+console.log(`Employee ID: ${EmpId} \n Employee Name:${EmpName} \n Employee Job: ${EmployeeJob} \n Is Permanent Employee:${IsPermanentEmployee} \n Employee Salary:${EmpSalary} \n  Employee DeptNo: ${EmpDeptNo}`);
