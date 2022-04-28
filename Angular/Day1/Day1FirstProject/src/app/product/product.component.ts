2dcf138409e43b8879053e2b00fc961a46489f92
prachiwabale03 <104131488+prachiwabale03@users.noreply.github.com>
2022-04-28 20:15:43 +0530

    Update product.component.ts

--- Angular/Day1/Day1FirstProject/src/app/product/product.component.ts
+++ Angular/Day1/Day1FirstProject/src/app/product/product.component.ts
@@ -8,14 +8,14 @@ import { Product } from 'src/Models/Product';
 })
 export class ProductComponent implements OnInit {
 
-  productObj1:Product=new Product(1,"Ball",100,"Games",10);
-  productObj2:Product=new Product(2,"Bat",100,"Games",10);
-  productObj3:Product=new Product(1,"Ball",100,"Games",10);
-  productObj4:Product=new Product(1,"Ball",100,"Games",10);
-  productObj5:Product=new Product(1,"Ball",100,"Games",10);
-  productObj6:Product=new Product(1,"Ball",100,"Games",10);
-  productObj7:Product=new Product(1,"Ball",100,"Games",10);
-  productObj8:Product=new Product(1,"Ball",100,"Games",10);
+  productObj1:Product=new Product(1,"Ball",100,"Sports",10);
+  productObj2:Product=new Product(2,"TV",10000,"Electronics",10);
+  productObj3:Product=new Product(3,"Bat",10008,"Sports",10);
+  productObj4:Product=new Product(4,"racket",1001,"Sports",10);
+  productObj5:Product=new Product(5,"Fan",100,"Electronics",10);
+  productObj6:Product=new Product(6,"Cap",10,"Sports",10);
+  productObj7:Product=new Product(7,"AC",150000,"Electronics",10);
+  productObj8:Product=new Product(8,"Battery",1000,"Electronics",10);
   productObj:Product[]=[this.productObj1,this.productObj2,this.productObj4,this.productObj5,this.productObj6,this.productObj7,this.productObj8];
 
   constructor() { }
