7931916a84fdc7fb3ca0315f5caf8f662faa1e51
prachiwabale03 <104131488+prachiwabale03@users.noreply.github.com>
2022-05-11 14:44:54 +0530

    Update grade.pipe.spec.ts

--- Jasmine_Karma/Day2/Task1/grade.pipe.spec.ts
+++ Jasmine_Karma/Day2/Task1/grade.pipe.spec.ts
@@ -5,36 +5,44 @@ describe('GradePipe', () => {
   beforeEach(() => {
     pipe = new GradePipe();
   });
+  //test instance creation
   it('create an instance', () => {
     const pipe = new GradePipe();
     expect(pipe).toBeTruthy();
   });
 
+  //test valid grade 1
   it('should return Outstanding when input is  1 ', () => {
     const result:string  = pipe.transform(1);
     expect(result).toBe("Outstanding");
   });
+
+    //test valid grade 2
   it('should return Excellent when input is  2 ', () => {
     const result:string  = pipe.transform(2);
     expect(result).toBe("Excellent");
   });
 
+    //test valid grade 3
   it('should return Good when input is  4 ', () => {
     const result:string  = pipe.transform(4);
     expect(result).toBe("Good");
   });
 
+    //test valid grade 4
   it('should return Average when input is  6 ', () => {
     const result:string  = pipe.transform(6);
     expect(result).toBe("Average");
   });
 
-  it('should return empty when input is invalid marks', () => {
+    //test invalid grade 1
+  it('should return empty when input is greater than 10', () => {
     const result:string  = pipe.transform(18);
     expect(result).toBe("");
   });
 
-  it('should return empty when input is invalid marks', () => {
+      //test invalid grade 2
+  it('should return empty when input is -1', () => {
     const result:string  = pipe.transform(-1);
     expect(result).toBe("");
   });
